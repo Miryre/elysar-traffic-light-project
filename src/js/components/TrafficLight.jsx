566dc7b3a9673701c7da480ea204ceac0126e680
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2026-03-18 13:28:42 -0500

    working traffic light +

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -2,12 +2,30 @@ import React, { useState } from "react";
 const TrafficLight = () => {
   //setting up useState
   const [currentLight, setCurrentLight] = useState("red");
+
   return (
     <>
       <div id="container">
-        <div className="red light"></div>
-        <div className="yellow light"></div>
-        <div className="green light"></div>
+        <div
+          className={`red light ${currentLight === "red" ? "active" : ""}`}
+          onClick={() => setCurrentLight("red")}
+        ></div>
+        <div
+          className={`yellow light ${
+            currentLight === "yellow" ? "active" : ""
+          }`}
+          onClick={() => setCurrentLight("yellow")}
+        ></div>
+        <div
+          className={`green light ${currentLight === "green" ? "active" : ""}`}
+          onClick={() => setCurrentLight("green")}
+        ></div>
+        <div
+          className={`purple light ${
+            currentLight === "purple" ? "active" : ""
+          }`}
+          onClick={() => setCurrentLight("purple")}
+        ></div>
       </div>
     </>
   );
